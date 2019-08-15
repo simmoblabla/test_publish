95d0f96f499e08bd97daecd5e51ab1573a977d0b
simmoblabla <simmoblabla@gmail.com>
2019-08-15 16:35:26 +0200

    Initial commit

---
+++ src/main/java/Operation.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+
+public class Operation {
+
+    public static BigDecimal somma(BigDecimal a, BigDecimal b){
+        return a.add(b);
+    }
+
+    public static BigDecimal sottrai(BigDecimal a, BigDecimal b){
+        return a.subtract(b);
+    }
+}
